b9b283e3a20d245398e0fe2a80535ce8bbbde95c
“hardik” <hardik.s.patel@cygnetinfotech.com>
2021-01-12 12:45:31 +0530

    Initial Commit

---
+++ TheOneTechnology/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheOneTechnology
+//
+//  Created by Apple on 12/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
